32b9d214e5ebc9f9493ea4e4263815d3de91b3af
s345871 <s345871@oslomet.no>
2020-10-20 18:34:08 +0200

    Fikk til oppgave 2, etter mye slit. Starter på oppgave 3

--- src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
+++ src/no/oslomet/cs/algdat/Eksamen/EksamenSBinTre.java
@@ -114,7 +114,27 @@ public class EksamenSBinTre<T> {
     }
 
     public int antall(T verdi) {
-        throw new UnsupportedOperationException("Ikke kodet ennå!");
+        if(antall()>0 && inneholder(verdi)){
+            Node <T> node = rot;
+            int teller = 0;
+            while(node!=null) {
+                if(comp.compare(verdi,node.verdi)==0){
+                    teller++;
+                    node=node.høyre;
+                }
+                else if(comp.compare(verdi,node.verdi)>0){
+                    node=node.høyre;
+                }else{
+                    node=node.venstre;
+                }
+            }
+
+            return teller;
+        }else{
+            return 0;
+        }
+
+
     }
 
     public void nullstill() {
@@ -149,12 +169,12 @@ public class EksamenSBinTre<T> {
         throw new UnsupportedOperationException("Ikke kodet ennå!");
     }
 
+
     public static void main(String[] args) {
         /*//oppgave 0
         EksamenSBinTre<String> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
         System.out.println(tre.antall()); // Utskrift: 0;
 
-         */
 
         //oppgave 1
         Integer[] a = {4,7,2,9,5,10,8,1,3,6};
@@ -162,6 +182,19 @@ public class EksamenSBinTre<T> {
         for (int verdi : a) tre.leggInn(verdi);
         System.out.println(tre.antall()); // Utskrift: 10
 
+
+
+        //oppgave 2
+        Integer[] a = {4,7,2,9,4,10,8,7,4,6};
+        EksamenSBinTre<Integer> tre = new EksamenSBinTre<>(Comparator.naturalOrder());
+        for (int verdi : a) tre.leggInn(verdi);
+        System.out.println(tre.antall()); // Utskrift: 10
+        System.out.println(tre.antall(5)); // Utskrift: 0
+        System.out.println(tre.antall(4)); // Utskrift: 3
+        System.out.println(tre.antall(7)); // Utskrift: 2
+        System.out.println(tre.antall(10)); // Utskrift: 1
+
+         */
 
 
     }
